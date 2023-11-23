be37d4b0ed123c6f154dafde5668a8036b7fdd31
kkukileon305 <kkukileon305@gmail.com>
2023-11-23 21:13:57 +0900

    add external modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,11 @@ plugins {
 
 android {
   namespace = "com.goodness.todolist"
-  compileSdk = 33
+  compileSdk = 34
 
   defaultConfig {
     applicationId = "com.goodness.todolist"
     minSdk = 26
-    targetSdk = 33
     versionCode = 1
     versionName = "1.0"
 
@@ -30,6 +29,9 @@ android {
   kotlinOptions {
     jvmTarget = "1.8"
   }
+  buildFeatures {
+    viewBinding = true
+  }
 }
 
 dependencies {
@@ -38,6 +40,7 @@ dependencies {
   implementation("androidx.appcompat:appcompat:1.6.1")
   implementation("com.google.android.material:material:1.10.0")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+  implementation("androidx.viewpager2:viewpager2:1.0.0")
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
